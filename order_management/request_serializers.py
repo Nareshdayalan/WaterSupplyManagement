a2ab9a50e8568c7de408e79895e1f2bd21f07b37
naresh <nareshdayalan1106@gmail.com>
2024-08-07 13:11:44 +0530

    changes to filterred

--- order_management/request_serializers.py
+++ order_management/request_serializers.py
@@ -19,3 +19,11 @@ class AddPaymentSerializer(serializers.Serializer):
     date_paid = serializers.DateTimeField(required = True)
     customer_id = serializers.UUIDField(required = True)
     order_id =  serializers.UUIDField(required = True)
+
+
+
+class AddPaymentSerializer(serializers.Serializer):
+    amount = serializers.IntegerField(required = True)
+    date_paid = serializers.DateTimeField(required = True)
+    customer_id = serializers.UUIDField(required = True)
+    order_id =  serializers.UUIDField(required = True)

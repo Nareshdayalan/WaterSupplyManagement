eba3b9fba72c1b014f35054b450fbdf434a23375
yugendar <yukeyabi@gmail.com>
2024-08-07 13:01:33 +0530

    api

--- order_management/views.py
+++ order_management/views.py
@@ -103,6 +103,19 @@ class GetPaymentDetails(APIView):
         return Response(serializer_payment.data)          
 
 
-        
+class GetPaymentDetails(APIView):
+    def post(self,request):
+        data = request.data
+        payment_data = Payment.objects.get(id = data['id'])
+        serializer_payment = PaymentSerializers(payment_data)
+        return Response(serializer_payment.data)  
+
+
+class GetPaymentDetails(APIView):
+    def post(self,request):
+        data = request.data
+        payment_data = Payment.objects.get(id = data['id'])
+        serializer_payment = PaymentSerializers(payment_data)
+        return Response(serializer_payment.data)        
 
     
